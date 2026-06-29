3a9a2298843dd3736a034db4ce99b72ffcee7739
name00-06 <kahoushusei11@gmail.com>
2026-06-29 20:09:50 +0900

    Fix world matrix assignment in script.js

--- script.js
+++ script.js
@@ -275,7 +275,7 @@ onload = function()
 
       // モデルのワールド行列の生成【ここをなんとかする】
       a_lMatrix[0] = a_wMatrix[1];
-      a_wMatrix[1] = a_lMatrix[0];
+      a_wMatrix[1] = a_wMatrix[0];
 	    
       // モデル描画
       gl.useProgram(prg_skin);
